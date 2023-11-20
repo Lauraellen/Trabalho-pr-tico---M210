d5c3a1c98bb7757a18d2ebaf52f10895693df039
laura.souza <laura.souza@gclaims.com.br>
2023-11-20 15:56:10 -0300

    valores otimos

--- Simplex.py
+++ Simplex.py
@@ -22,7 +22,7 @@ def createFunction(variaveis):
         if i < len(variaveis) - 1:
             funcao_objetivo += " + "
 
-    return funcao_objetivo
+    return funcao_objetivo, coeficientes
 
 def getRestricts(variaveis):
     num_restricoes = int(input("Quantas restrições você deseja adicionar? "))
@@ -30,32 +30,57 @@ def getRestricts(variaveis):
     restricoes = []
 
     for i in range(num_restricoes):
-        expressao = ""
+        expressao = []
         for j in range(len(variaveis)):
             coeficiente = float(input(f"Digite o coeficiente para a variável {variaveis[j]} na restrição {i + 1}: "))
-            expressao += f"{coeficiente} * {variaveis[j]}"
-
-            if j < len(variaveis) - 1:
-                expressao += " + "
+            expressao.append(coeficiente)
 
         sinal = input(f"Digite o sinal de desigualdade (<= ou >=) para a restrição {i + 1}: ")
         valor = float(input(f"Digite o valor para a restrição {i + 1}: "))
-        expressao += f" {sinal} {valor}"
 
+        # Adiciona os coeficientes, sinal e valor à lista da restrição
+        expressao.extend([sinal, valor])
+
+        # Adiciona a lista da restrição à lista de restrições
         restricoes.append(expressao)
 
     return restricoes
 
+
 # Exemplo de uso
 variaveis_decisao = addVariablesDecision()
-funcao_objetivo_resultante = createFunction(variaveis_decisao)
-print("Função objetivo resultante:", funcao_objetivo_resultante)
-
+funcao_objetivo_resultante, coeficientes = createFunction(variaveis_decisao)
 restricoes_resultantes = getRestricts(variaveis_decisao)
+variaveisAuxiliares = []
 
-print("Restrições resultantes:")
-for restricao in restricoes_resultantes:
-    print(restricao)
+def getGreatValues():
+    print(variaveis_decisao)
+    print(funcao_objetivo_resultante)
+    print(coeficientes)
+    print(restricoes_resultantes)
+
+    funcao_objetivo = [-coeficiente for coeficiente in coeficientes]
+    funcao_objetivo.append(0)
+    restricoes_na_forma_canonica = [converter_restricoes_para_forma_canonica(restricao) for restricao in restricoes_resultantes]
+    print(restricoes_na_forma_canonica)
+    print(funcao_objetivo)
 
 
+
+def converter_restricoes_para_forma_canonica(restricao):
+    restricao_na_forma_canonica = restricao.copy()
+    simbolo_de_desigualdade = restricao[-2]
+    prox_variavel_auxiliar = variaveisAuxiliares[-1][: -1] + str(len(variaveisAuxiliares) + 1) if len(
+        variaveisAuxiliares) > 0 else "aux1"
+    variaveisAuxiliares.append(prox_variavel_auxiliar)
+
+    if simbolo_de_desigualdade == "<=":
+        restricao_na_forma_canonica[-2] = 1
+    elif simbolo_de_desigualdade == ">=":
+        restricao_na_forma_canonica[-2] = -1
+
+    return restricao_na_forma_canonica
+
+
+getGreatValues()
 
