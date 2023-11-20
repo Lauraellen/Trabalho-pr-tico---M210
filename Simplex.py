8219531bf12f80b638872252fb0972a143ba133e
laura.souza <laura.souza@gclaims.com.br>
2023-11-20 20:38:39 -0300

    valor otimo de cada variavel

--- Simplex.py
+++ Simplex.py
@@ -52,19 +52,15 @@ variaveis_decisao = addVariablesDecision()
 funcao_objetivo_resultante, coeficientes = createFunction(variaveis_decisao)
 restricoes_resultantes = getRestricts(variaveis_decisao)
 variaveisAuxiliares = []
+quadros = []
 
 def getGreatValues():
-    print(variaveis_decisao)
-    print(funcao_objetivo_resultante)
-    print(coeficientes)
-    print(restricoes_resultantes)
-
     funcao_objetivo = [-coeficiente for coeficiente in coeficientes]
     funcao_objetivo.append(0)
     restricoes_na_forma_canonica = [converter_restricoes_para_forma_canonica(restricao) for restricao in restricoes_resultantes]
-    print(restricoes_na_forma_canonica)
-    print(funcao_objetivo)
 
+    gerar_novo_quadro({}, funcao_objetivo, restricoes_na_forma_canonica)
+    return obter_valores_otimos();
 
 
 def converter_restricoes_para_forma_canonica(restricao):
@@ -82,5 +78,124 @@ def converter_restricoes_para_forma_canonica(restricao):
     return restricao_na_forma_canonica
 
 
-getGreatValues()
+def calcular_valores_nova_linha(quadro_antigo, indice_linha, indice_coluna_pivo, indice_linha_pivo):
+    valor_elemento_pivo = quadro_antigo["valores"][indice_linha_pivo][indice_coluna_pivo]
+    linha_referencia = [valor / valor_elemento_pivo for valor in quadro_antigo["valores"][indice_linha_pivo]]
+    new_linha = []
+
+    if indice_linha == indice_linha_pivo:
+        new_linha = linha_referencia
+    else:
+        new_linha = [valor + (-1 * quadro_antigo["valores"][indice_linha][indice_coluna_pivo] * linha_referencia[index])
+                     for index, valor in enumerate(quadro_antigo["valores"][indice_linha])]
+
+    return new_linha
+
+def obter_valores_otimos():
+    quadro_novo = quadros[-1]
+    continuar_iteracao = verificar_se_ainda_ha_elementos_negativos(quadro_novo)
+
+    if not continuar_iteracao:
+        return obter_valores_otimos_pelo_quadro(quadro_novo)
+    elif len(quadros) == 100:
+        quadro_com_maior_lucro = None
+        maior_lucro = 0
+
+        for quadro in quadros:
+            if quadro["valores"][0][-1] > maior_lucro:
+                maior_lucro = quadro["valores"][0][-1]
+                quadro_com_maior_lucro = quadro
+
+        return obter_valores_otimos_pelo_quadro(quadro_com_maior_lucro)
+    else:
+        elemento_pivo = obter_elemento_pivo(quadro_novo)
+        gerar_novo_quadro(elemento_pivo)
+        return obter_valores_otimos()
+
+def verificar_se_ainda_ha_elementos_negativos(quadro):
+    return any(elemento < 0 for elemento in quadro["valores"][0])
+
+def obter_valores_otimos_pelo_quadro(ultimo_quadro):
+    valores_otimos = []
+
+    for variavel in variaveis_decisao:
+        index_valor_otimo_variavel = ultimo_quadro["linhas"].index(variavel) if variavel in ultimo_quadro["linhas"] else -1
+
+        if index_valor_otimo_variavel != -1:
+            valor_otimo_variavel = ultimo_quadro["valores"][index_valor_otimo_variavel][-1]
+            valores_otimos.append({
+                "variavel": variavel,
+                "valor_otimo": valor_otimo_variavel
+            })
+        else:
+            valores_otimos.append({
+                "variavel": variavel,
+                "valor_otimo": 0
+            })
+
+    return valores_otimos
+
+def obter_elemento_pivo(quadro):
+    indice_coluna_pivo = quadro["valores"][0].index(min(quadro["valores"][0]))
+    relacao_ld_com_a_coluna_pivo = []
+    for restricao in quadro["valores"]:
+        valor = restricao[-1] / restricao[indice_coluna_pivo] \
+            if restricao[indice_coluna_pivo] > 0 else float('inf')
+        relacao_ld_com_a_coluna_pivo.append(valor)
+
+    menor_valor = min(relacao_ld_com_a_coluna_pivo)
+    indice_linha_pivo = relacao_ld_com_a_coluna_pivo.index(menor_valor)
+
+    return {"indiceColunaPivo": indice_coluna_pivo, "indiceLinhaPivo": indice_linha_pivo}
+
+def gerar_novo_quadro(elemento_pivo, funcao_objetivo=None, restricoes_na_forma_canonica=None):
+
+
+    if not quadros:
+        num_variaveis_auxiliares = len(variaveisAuxiliares)
+
+        quadros.append({
+            "valores": [funcao_objetivo]
+        })
+
+        quadros[0]["colunas"] = variaveis_decisao + variaveisAuxiliares
+        quadros[0]["linhas"] = ["Z"] + variaveisAuxiliares
+
+        for _ in range(num_variaveis_auxiliares):
+            quadros[0]["valores"][0].append(0)
+
+        for index, restricao in enumerate(restricoes_na_forma_canonica):
+            quadros[0]["valores"].append(restricao.copy())
+
+            if index == 0:
+                for _ in range(num_variaveis_auxiliares - 1):
+                    quadros[0]["valores"][-1].insert(-1, 0)
+            else:
+                for i in range(index):
+                    quadros[0]["valores"][-1].insert(-2, 0)
+
+                for _ in range(num_variaveis_auxiliares - index - 1):
+                    quadros[0]["valores"][-1].insert(-1, 0)
+    else:
+        ultima_posicao = len(quadros)
+        new_quadro = {
+            "colunas": quadros[ultima_posicao - 1]["colunas"][:],
+            "linhas": quadros[ultima_posicao - 1]["linhas"][:],
+            "valores": [linha[:] for linha in quadros[ultima_posicao - 1]["valores"]]  # Cria cópias das listas internas
+        }
+
+        new_quadro["colunas"][elemento_pivo["indiceColunaPivo"]] = quadros[ultima_posicao - 1]["linhas"][
+            elemento_pivo["indiceLinhaPivo"]]
+        new_quadro["linhas"][elemento_pivo["indiceLinhaPivo"]] = quadros[ultima_posicao - 1]["colunas"][
+            elemento_pivo["indiceColunaPivo"]]
+
+        for index, linha in enumerate(new_quadro["valores"]):
+            new_quadro["valores"][index] = calcular_valores_nova_linha(quadros[ultima_posicao - 1], index,
+                                                                       elemento_pivo["indiceColunaPivo"],
+                                                                       elemento_pivo["indiceLinhaPivo"])
+
+        quadros.append(new_quadro)
+
+teste = getGreatValues()
+print('VALORES OTIMOS => ', teste)
 
