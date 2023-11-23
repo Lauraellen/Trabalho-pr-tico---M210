25224fd3f5726a5559f30021f6ef18e7d0247bee
laura.souza <laura.souza@gclaims.com.br>
2023-11-22 21:46:56 -0300

    add comments

--- Simplex.py
+++ Simplex.py
@@ -51,6 +51,8 @@ def getRestricts(variaveis):
 
 
 def getGreatValues():
+
+    #forma padrão
     funcao_objetivo = [-coeficiente for coeficiente in coeficientes]
     funcao_objetivo.append(0)
     restricoes_na_forma_canonica = [converter_restricoes_para_forma_canonica(restricao) for restricao in restricoes_resultantes]
@@ -76,6 +78,9 @@ def converter_restricoes_para_forma_canonica(restricao):
 
 def calcular_valores_nova_linha(quadro_antigo, indice_linha, indice_coluna_pivo, indice_linha_pivo):
     valor_elemento_pivo = quadro_antigo["valores"][indice_linha_pivo][indice_coluna_pivo]
+
+    #valor da linha onde se encontra o pivô / valor do pivô - linha referencia
+    #valor da linha anterior + (-1)valorLinhaColuna*valores da linha de referencia
     linha_referencia = [valor / valor_elemento_pivo for valor in quadro_antigo["valores"][indice_linha_pivo]]
     new_linha = []
 
@@ -91,6 +96,8 @@ def obter_valores_otimos():
     quadro_novo = quadros[-1]
     continuar_iteracao = verificar_se_ainda_ha_elementos_negativos(quadro_novo)
 
+    # Colocamos a restrição para caso o len == 100, para limitar
+    #casso ocorra alguma execução infinita
     if not continuar_iteracao:
         return obter_valores_otimos_pelo_quadro(quadro_novo)
     elif len(quadros) == 100:
@@ -115,8 +122,13 @@ def obter_valores_otimos_pelo_quadro(ultimo_quadro):
     valores_otimos = []
 
     for variavel in variaveis_decisao:
-        index_valor_otimo_variavel = ultimo_quadro["linhas"].index(variavel) if variavel in ultimo_quadro["linhas"] else -1
 
+        #se a varivel de decisão se encontra no quadro
+        #seu valor ótimo será a ultima posição da linha dessa variavel que corresponde
+        #ao seu valor ótimo
+
+        #se não estiver no quadro, seu valor é 0
+        index_valor_otimo_variavel = ultimo_quadro["linhas"].index(variavel) if variavel in ultimo_quadro["linhas"] else -1
         if index_valor_otimo_variavel != -1:
             valor_otimo_variavel = ultimo_quadro["valores"][index_valor_otimo_variavel][-1]
             valores_otimos.append({
@@ -134,6 +146,8 @@ def obter_valores_otimos_pelo_quadro(ultimo_quadro):
 def obter_elemento_pivo(quadro):
     indice_coluna_pivo = quadro["valores"][0].index(min(quadro["valores"][0]))
     relacao_ld_com_a_coluna_pivo = []
+
+    #o valor depois do sinal >=|<= dividido pelo pivô
     for restricao in quadro["valores"]:
         valor = restricao[-1] / restricao[indice_coluna_pivo] \
             if restricao[indice_coluna_pivo] > 0 else float('inf')
@@ -145,8 +159,6 @@ def obter_elemento_pivo(quadro):
     return {"indiceColunaPivo": indice_coluna_pivo, "indiceLinhaPivo": indice_linha_pivo}
 
 def gerar_novo_quadro(elemento_pivo, funcao_objetivo=None, restricoes_na_forma_canonica=None):
-
-
     if not quadros:
         num_variaveis_auxiliares = len(variaveisAuxiliares)
 
@@ -157,9 +169,11 @@ def gerar_novo_quadro(elemento_pivo, funcao_objetivo=None, restricoes_na_forma_c
         quadros[0]["colunas"] = variaveis_decisao + variaveisAuxiliares
         quadros[0]["linhas"] = ["Z"] + variaveisAuxiliares
 
+        #adicionando zeros na linha Z, nas colunas das variaveis auxiliares
         for _ in range(num_variaveis_auxiliares):
             quadros[0]["valores"][0].append(0)
 
+        #adicionando zeros nas respectivas auxiliares
         for index, restricao in enumerate(restricoes_na_forma_canonica):
             quadros[0]["valores"].append(restricao.copy())
 
@@ -174,12 +188,16 @@ def gerar_novo_quadro(elemento_pivo, funcao_objetivo=None, restricoes_na_forma_c
                     quadros[0]["valores"][-1].insert(-1, 0)
     else:
         ultima_posicao = len(quadros)
+
+        #criando uma cópia do quadro anterior
         new_quadro = {
             "colunas": quadros[ultima_posicao - 1]["colunas"][:],
             "linhas": quadros[ultima_posicao - 1]["linhas"][:],
             "valores": [linha[:] for linha in quadros[ultima_posicao - 1]["valores"]]  # Cria cópias das listas internas
         }
 
+        #atualizo o novo quadro com base no pivô encontrado
+        #fazendo a troca da linha com a coluna onde se encontra o pivô
         new_quadro["colunas"][elemento_pivo["indiceColunaPivo"]] = quadros[ultima_posicao - 1]["linhas"][
             elemento_pivo["indiceLinhaPivo"]]
         new_quadro["linhas"][elemento_pivo["indiceLinhaPivo"]] = quadros[ultima_posicao - 1]["colunas"][
@@ -189,25 +207,24 @@ def gerar_novo_quadro(elemento_pivo, funcao_objetivo=None, restricoes_na_forma_c
             new_quadro["valores"][index] = calcular_valores_nova_linha(quadros[ultima_posicao - 1], index,
                                                                        elemento_pivo["indiceColunaPivo"],
                                                                        elemento_pivo["indiceLinhaPivo"])
-
         quadros.append(new_quadro)
 
 
 def obter_lucro_total(valores_otimos):
+    #coeficientes adiciionados * o valor ótimo de cada variavel de decisão
     lucro_total = 0
     for indice in valores_otimos:
         indice_variavel = variaveis_decisao.index(indice["variavel"])
         lucro_total += coeficientes[indice_variavel] * indice["valor_otimo"]
     return lucro_total
 
-
-
 def obter_preco_sombra():
-    # valores_otimos = gerar_valores_otimos(variaveis_input, coeficientes_fo_input, restricoes_input)
     ultimo_quadro = quadros[-1]
     precos_sombra = []
     for index, valor in enumerate(ultimo_quadro["valores"][0]):
 
+        #verifica as colunas onde estão as variaveis auxiliares
+        #o primeiro valor de cada variavel auxiliar, corresponde ao preço sombra da mesma
         if ultimo_quadro["colunas"][index-1] and index >= (len(ultimo_quadro["valores"][0]) - len(variaveisAuxiliares) - 1):
             precos_sombra.append({
                 "variavel": ultimo_quadro["colunas"][index-1],
@@ -223,7 +240,8 @@ def obter_preco_sombra():
 
 def get_new_values(restricoes_nome):
     new_values = []
-    # Recebe valores do usuário de acordo com o tamanho fornecido
+    # Recebe valores do usuário de acordo com o quantidade de restrições
+
     for i, value in enumerate(restricoes_nome):
         novo_valor = float(input(f"Digite o novo valor para {value}: "))
         new_values.append(novo_valor)
@@ -265,7 +283,9 @@ def verificar_viabilidade(ultimo_quadro, variacoes_de_disponibilidade):
 # Exemplo de uso
 variaveis_decisao = addVariablesDecision()
 funcao_objetivo_resultante, coeficientes = createFunction(variaveis_decisao)
+print('Função objetivo: ', funcao_objetivo_resultante)
 restricoes_resultantes, restricoes_nome = getRestricts(variaveis_decisao)
+print('Restrições: ', restricoes_resultantes)
 variaveisAuxiliares = []
 quadros = []
 
@@ -279,12 +299,12 @@ print('Lucro total => ', lucro_total)
 preco_sombra = obter_preco_sombra()
 print('Preço sombra => ', preco_sombra)
 
-alterar_disponibilidade = int(input("Deseja alterar a disponibilidade de algum recurso? 1 - SIM | 0 - NÃO"))
+alterar_disponibilidade = int(input("Deseja alterar a disponibilidade de algum recurso? 1 - SIM | 0 - NÃO "))
 
 if (alterar_disponibilidade == 1):
     new_values = get_new_values(restricoes_nome)
     novo_lucro, viavel = verificar_viabilidade_e_novo_lucro(new_values)
-    print("É viável" if viavel else "Não é viavel")
+    print("É viável" if viavel else "Não é viável")
     print("Novo lucro ", novo_lucro+lucro_total)
     print("Diferença ", novo_lucro)
 
